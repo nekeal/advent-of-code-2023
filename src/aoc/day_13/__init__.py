4b058aa2fd295d8aa997067992059c72104bdea0
Szymon Cader <szymon.sc.cader@gmail.com>
2023-12-15 21:49:36 +0100

    Solution day 13

---
+++ src/aoc/day_13/__init__.py
@@ -0,0 +1,67 @@
+import operator
+from functools import reduce
+from typing import Generator
+
+from aoc.base import BaseChallenge
+
+
+class Matrix:
+    def __init__(self, matrix: list[str]):
+        self.matrix = matrix
+
+    @property
+    def height(self):
+        return len(self.matrix)
+
+    @property
+    def width(self):
+        return len(self.matrix[0])
+
+    def find_mirrors(self, smudges: int) -> Generator[int, None, None]:
+        for c in range(self.width - 1):
+            badness = 0
+            for dc in range(min(c + 1, self.width - c - 1)):
+                for r in range(self.height):
+                    badness += self.matrix[r][c - dc] != self.matrix[r][c + dc + 1]
+            if badness == smudges:
+                yield c + 1
+        for r in range(self.height - 1):
+            badness = 0
+            for dr in range(min(r + 1, self.height - r - 1)):
+                for c in range(self.width):
+                    badness += self.matrix[r - dr][c] != self.matrix[r + dr + 1][c]
+            if badness == smudges:
+                yield (r + 1) * 100
+
+
+class Challenge(BaseChallenge):
+    def part_1(self):
+        return reduce(
+            operator.add,
+            (
+                val
+                for matrix in self.get_matrixes(part=1)
+                for val in matrix.find_mirrors(smudges=0)
+            ),
+        )
+
+    def part_2(self):
+        return reduce(
+            operator.add,
+            (
+                val
+                for matrix in self.get_matrixes(part=2)
+                for val in matrix.find_mirrors(smudges=1)
+            ),
+        )
+
+    def get_matrixes(self, part) -> Generator[Matrix, None, None]:
+        yield from (
+            Matrix(matrix.splitlines())
+            for matrix in "\n".join(self.get_input_lines(part=part)).split("\n\n")
+        )
+
+
+if __name__ == "__main__":
+    Challenge().run()
+    Challenge(use_test_data=True).run()
